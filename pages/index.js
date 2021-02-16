07e482c56ea834ea14426ae4c77c1ccbd5999f6a
Wilson Santos <contato@wilsonsantos.net>
2021-02-16 15:31:19 -0300

    :zap: Change html components for Chakra components
    change components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Box, Code, Heading, Link, Text, Flex } from "@chakra-ui/react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,43 +11,85 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Heading as="h1" size="2xl" mb="2">
+          Welcome to{" "}
+          <Link color="purple.200" href="https://nextjs.org">
+            Next.js!
+          </Link>
+        </Heading>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Text fontSize="2xl" mt="3">
+          Get started by editing <Code> pages/index.js </Code>
+        </Text>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Box
+            as="a"
+            href="https://nextjs.org/docs"
+            p="6"
+            m="4"
+            borderWidth="1px"
+            rounded="lg"
+            flexBasis="45%"
+          >
+            <Heading as="h3" size="lg" mb="2">
+              Documentation &rarr;
+            </Heading>
+            <Text fontSize="lg">
+              Find in-depth information about Next.js features and API.
+            </Text>
+          </Box>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Box
+            as="a"
+            href="https://nextjs.org/learn"
+            p="6"
+            m="4"
+            borderWidth="1px"
+            rounded="lg"
+            flexBasis="45%"
+          >
+            <Heading as="h3" size="lg" mb="2">
+              Learn &rarr;
+            </Heading>
+            <Text fontSize="lg">
+              Learn about Next.js in an interactive course with quizzes!
+            </Text>
+          </Box>
 
-          <a
+          <Box
+            as="a"
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            p="6"
+            m="4"
+            borderWidth="1px"
+            rounded="lg"
+            flexBasis="45%"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <Heading as="h3" size="lg" mb="2">
+              Examples &rarr;
+            </Heading>
+            <Text fontSize="lg">
+              Discover and deploy boilerplate example Next.js projects.
+            </Text>
+          </Box>
 
-          <a
+          <Box
+            as="a"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            p="6"
+            m="4"
+            borderWidth="1px"
+            rounded="lg"
+            flexBasis="45%"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
+            <Heading as="h3" size="lg" mb="2">
+              Deploy &rarr;
+            </Heading>
+            <Text fontSize="lg">
               Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            </Text>
+          </Box>
         </div>
       </main>
 
@@ -56,10 +99,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
