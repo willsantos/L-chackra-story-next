2b7b34a7a525f9010dea7818730ebac278d7d561
Wilson Santos <contato@wilsonsantos.net>
2021-02-16 15:37:57 -0300

    :sparkles: Create a custom theme
    Create a custom theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { ThemeProvider, theme, CSSReset } from "@chakra-ui/react";
+import "../styles/globals.css";
+
+const customTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    purple: {
+      50: "#f5e9ff",
+      100: "#dac1f3",
+      200: "#c098e7",
+      300: "#a571dc",
+      400: "#8c48d0",
+      500: "#722fb7",
+      600: "#59238f",
+      700: "#3f1968",
+      800: "#260f40",
+      900: "#10031a",
+    },
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
